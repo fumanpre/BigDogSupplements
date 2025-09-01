8921219ad40cf5bbefb34a8534c0e48cdb2d8d1c
. Fumanpreet Singh <fumanpre@ualberta.ca>
2025-08-31 19:09:43 -0600

    hello

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -3,12 +3,12 @@ import { notFound } from 'next/navigation'
 import ProductCategoryDisplay from '../../components/ProductCategoryDisplay'
 import ProductClient from './ProductClient' // client component
 
-interface Params {
+interface ProductPageParams {
   id: string
 }
 
 interface ProductPageProps {
-  params: Params
+  params: ProductPageParams
 }
 
 async function getProduct(id: string) {
@@ -21,22 +21,19 @@ async function getProduct(id: string) {
   return product
 }
 
-// Use destructuring of params with type annotation
+// ✅ Correctly typed async server component
 export default async function ProductPage({ params }: ProductPageProps) {
   const { id } = params
   const product = await getProduct(id)
 
-  // Sort flavors by popularity
   const sortedFlavors = product.flavors.sort(
     (a, b) => (b.popularity || 0) - (a.popularity || 0)
   )
 
   return (
     <>
-      {/* Product Details */}
       <ProductClient product={product} flavors={sortedFlavors} />
 
-      {/* Similar & Manufacturer Products */}
       <div className="py-6 sm:py-10 lg:py-20">
         <ProductCategoryDisplay
           className="p-4"
