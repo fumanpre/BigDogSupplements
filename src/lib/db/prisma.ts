f0db041465ce64cd25b43c3b299e46419ed87e3e
. Fumanpreet Singh <fumanpre@ualberta.ca>
2025-09-01 19:11:43 -0600

    resolving issure related to import error from prisma/client

--- src/lib/db/prisma.ts
+++ src/lib/db/prisma.ts
@@ -1,5 +1,5 @@
 // lib/prisma.ts
-import { PrismaClient } from '../../../prisma/app/generated/prisma/client'
+import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
